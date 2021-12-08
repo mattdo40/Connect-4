f80d5ee3c293a02d5cc387182e421fb506da12cb
mattdo40 <75507361+mattdo40@users.noreply.github.com>
2021-12-07 23:40:26 -0800

    Add files via upload

---
+++ bots/__init__.py
@@ -0,0 +1,11 @@
+from .human import Human
+from .random import RandomBot
+from .minimax import MiniMaxBot
+from .montecarlo import MonteCarloBot
+
+__all__ = [
+    'Human',
+    'RandomBot',
+    'MiniMaxBot',
+    'MonteCarloBot'
+]
